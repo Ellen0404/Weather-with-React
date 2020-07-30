fac8bea865b7b10b5c973c4546edbd9690870f8f
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-07-29 23:25:07 -0600

    weather data displayed on Card Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, } from "reactstrap";
+import moment from "moment";
+import sampleData from "./data/sample.json";
 import SearchBar from "./components/SearchBar";
 import DayDetails from "./components/DayDetails";
 import DayCard from "./components/DayCard";
 
-function App() {
+const App = () => {
+
+  const [weatherinfo, setWeatherInfo] = useState({
+    searchTerm: "",
+    selectedDay: null,
+    days: sampleData.data,
+    location: "Denver, CO",
+    // days: [],
+    // location: ""
+
+  });
+
+  const { searchTerm, selectedDay, days, location } = weatherinfo;
+
   return (
     <Container>
       <Row>
         <Col md={7}>
-          <h1>Weather Info for TTTTT</h1>
+          <h1>Weather Info for {location}</h1>
         </Col>
         <Col md={5}>
           <SearchBar />
         </Col>
       </Row>
-      <Row></Row>
+      <Row>
+        {days.map(day => (
+          <DayCard
+            key={day.ts}
+            day={moment(day.valid_date, "YYYY-MM-DD").format("dddd")}
+            temp={day.temp}
+            icon={day.weather.icon}
+            description={day.weather.description}
+            high={day.high_temp}
+            low={day.low_temp}
+            precip={day.precip}
+
+          />
+        ))}
+      </Row>
       <Row>
         <Col>
           <DayDetails />
--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Col, Card, CardHeader, CardBody } from "reactstrap";
 
 const DayCard = props => {
     return (
-        <p>DayCard</p>
+        <Col>
+            <Card>
+                <CardHeader>{props.day}</CardHeader>
+                <CardBody>
+                    <h3>{props.temp}°</h3>
+                    <img src={`${process.env.PUBLIC_URL}/icons/${props.icon}.png`} alt={props.description} />
+                    <p><strong>High: {props.high}°</strong></p>
+                    <p><strong>Low: {props.low}°</strong></p>
+                    <p><strong>precip: {props.precip}%</strong></p>
+                </CardBody>
+            </Card>
+        </Col>
     )
 }
 
